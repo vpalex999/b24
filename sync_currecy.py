bc140e1f4b8346790c41df15ad84195301281070
vpalex999 <vpalex@mail.ru>
2021-10-14 23:13:15 +0500

    start dev sync_currency

---
+++ sync_currecy.py
@@ -0,0 +1,8 @@
+
+def sync_currency_pairs(currency_pairs, src_repo, dst_repo) -> bool:
+    print("it's work")
+    return True
+
+
+if __name__ == "__main__":
+    sync_currency_pairs("currency_pairs", "sber_repo", "b24_repo")
